18ad44c1f65c938e47a0fde578d54e47223a13da
talhashahid11 <142029999+talhashahid11@users.noreply.github.com>
2026-04-01 00:22:23 +0500

    Add get_flights function to retrieve flight data

---
+++ flight_api.py
@@ -0,0 +1,8 @@
+def get_flights():
+    return [
+        {"id": 1, "from": "Lahore", "to": "Dubai", "price": 500, "airline": "Emirates"},
+        {"id": 2, "from": "Islamabad", "to": "Dubai", "price": 450, "airline": "Air Arabia"},
+        {"id": 3, "from": "Karachi", "to": "Istanbul", "price": 700, "airline": "Turkish Airlines"},
+        {"id": 4, "from": "Lahore", "to": "London", "price": 900, "airline": "British Airways"},
+        {"id": 5, "from": "Karachi", "to": "Dubai", "price": 480, "airline": "FlyDubai"},
+    ]
